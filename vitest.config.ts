198f02f1d241c34c5b3cc101e7a7a00642466561
Tiago Ferreira <tiago@ferreira.io>
2024-04-25 00:16:17 -0700

    improve coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    include: ['packages/**/*.test.ts?(x)'],
     coverage: {
+      include: ['packages/**/src/**/*.ts?(x)'],
       reporter: ['text', 'json-summary', 'json'],
     },
   },
