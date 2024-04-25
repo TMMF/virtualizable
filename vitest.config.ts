c072835a357fe1c3b7bf3fd1da58fc62c9d75ed3
Tiago Ferreira <tiago@ferreira.io>
2024-04-25 00:34:15 -0700

    try not to use gh reporter in CI

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig, configDefaults } from 'vitest/config'
 
 export default defineConfig({
   test: {
     include: ['packages/**/*.test.ts?(x)'],
+    reporters: process.env.COVERAGE ? ['basic'] : configDefaults.reporters,
     coverage: {
       include: ['packages/**/src/**/*.ts?(x)'],
       reporter: ['text', 'json-summary', 'json'],
